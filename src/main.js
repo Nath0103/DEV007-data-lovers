d0fe36653a616801d16a4bf56eefce020512e851
Sue419 <suzan419@gmail.com>
2023-05-11 13:30:36 -0500

    cambios juego.html

--- src/main.js
+++ src/main.js
@@ -1,17 +1,2 @@
 //import { example } from "./data.js";
-import data from "./data/lol/lol.js";
-const card = [];
-const fullData = Object.keys(data.data);
-for (let i = 0; i < fullData.length; i++) {
-  const nombres = fullData[i];
-  const tituloCampeon = fullData.name;
-  console.log(tituloCampeon);
-  card.push(`
-  <h1 >
-  ${nombres}
-  </h1>
-  `);
-  const contenedorCampeones = document.getElementById("campeones");
-  contenedorCampeones.innerHTML = card.join("");
-  console.log(nombres);
-}
+
