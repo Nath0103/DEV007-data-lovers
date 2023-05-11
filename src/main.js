50f56ba24aabc1b38d0f68c18925ab7e35375581
Sue419 <suzan419@gmail.com>
2023-05-11 13:37:32 -0500

    cambio juego.html

--- src/main.js
+++ src/main.js
@@ -1,2 +1,36 @@
 //import { example } from "./data.js";
+import data from "./data/lol/lol.js";
+const card = [];
+const imgSize = 100;
+const fullData = Object.values(data.data); //tengo todos los datos limitamos la full data
+const arrayLimitado = fullData.slice(0, 19); //Limita toda la data
 
+arrayLimitado.forEach((element) => {
+  // element.slice(0, 19); //para limitarlo
+
+  card.push(`
+  <div>
+  <h2>${element.name}</h2>
+  <img src="${element.img}" alt="img campeones"  width="${imgSize}">
+  </div>
+  `);
+  const contenedorCampeones = document.getElementById("campeones-card");
+  contenedorCampeones.innerHTML = card.join("");
+});
+//MUETRAS TODOS LOS CAMPEONES👇
+// const btnCampeones = document.getElementById("todosLosCampeones");
+// btnCampeones.addEventListener("click", () => {
+//   fullData.forEach((element) => {
+//     // const element = data.data[i];
+//     // element.slice(0, 19); //para limitarlo
+
+//     card.push(`
+//     <div>
+//     <h2>${element.name}</h2>
+//     <img src="${element.img}" alt="img campeones"  width="${imgSize}">
+//     </div>
+//     `);
+//     const contenedorCampeones = document.getElementById("campeones-card");
+//     contenedorCampeones.innerHTML = card.join("");
+//   });
+// });
