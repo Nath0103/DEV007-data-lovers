43243e632e417e603348f95a4a33b61d01974da5
Nathaly Santos <nathalysantos0103@gmail.com>
2023-05-19 12:50:52 -0500

    cambios

--- src/juego.js
+++ src/juego.js
@@ -1,16 +1,26 @@
 import data from "./data/lol/lol.js";
-const fullData = Object.values(data.data); //tengo todos los datos limitamos la full data
-const imgSize = 100; //cambiar el tamaño
+// Obtenemos todos los datos del objeto data
+const fullData = Object.values(data.data);
 
-const cardPage3 = [];
+const imgSize = 100; // Tamaño de las imágenes de los campeones (puedes cambiarlo según las necesidades)
+
+const cardPage3 = []; // Array donde almacenaremos las tarjetas de los campeones
+
+// Limitamos la cantidad de campeones que queremos mostrar (en este caso, 4)
 const campeonesEnJuego = fullData.slice(0, 4);
+
+// Recorremos los campeones y creamos las tarjetas para cada uno
 campeonesEnJuego.forEach((element) => {
   cardPage3.push(`
-  <div>
-  <h3>${element.name}</h3>
-  <img src="${element.img}" alt="img campeones"  width="${imgSize}">
-  </div>
+    <div>
+      <h3>${element.name}</h3>
+      <img src="${element.img}" alt="img campeones" width="${imgSize}">
+    </div>
   `);
+
+  // Obtenemos el elemento del DOM donde queremos mostrar las tarjetas de los campeones
   const algunosCampeones = document.getElementById("algunosCampeones");
+
+  // Insertamos las tarjetas en el elemento del DOM y las unimos en una cadena de texto
   algunosCampeones.innerHTML = cardPage3.join("");
 });
--- src/main.js
+++ src/main.js
@@ -1,39 +1,75 @@
 //import { example } from "./data.js";
+// Importar datos desde "./data/lol/lol.js"
 import data from "./data/lol/lol.js";
-let card = [];
-const imgSize = 100;
-const fullData = Object.values(data.data); //tengo todos los datos limitamos la full data
-const arrayLimitado = fullData.slice(0, 20); //Limita toda la data
 
+// Crear un arreglo vacío llamado "card"
+let card = [];
+// Obtener los valores de la propiedad "data" del objeto "data"
+const fullData = Object.values(data.data);
+
+// Limitar los datos a un subconjunto de elementos
+const arrayLimitado = fullData.slice(0, 9);
+
+// Iterar sobre cada elemento del arreglo "arrayLimitado"
 arrayLimitado.forEach((element) => {
-  // element.slice(0, 19); //para limitarlo
+  // Agregar contenido HTML al arreglo "card" usando template literals
   card.push(`
-  <div>
-  <h2>${element.name}</h2>
-  <img src="${element.img}" alt="img campeones"  width="${imgSize}">
-  </div>
+    <div class="containerCard">
+      <section class="card">
+        <h2>${element.name}</h2>
+        <span>${element.title}</span>
+        <img src="${element.splash}" alt="img campeones" class="pic">
+        <button></button>
+      </section>
+    </div>
   `);
+
+  // Obtener el elemento del DOM con el ID "campeones-card"
   const contenedorCampeones = document.getElementById("campeones-card");
+
+  // Actualizar el contenido del elemento con el HTML generado por "card" utilizando el método "join"
   contenedorCampeones.innerHTML = card.join("");
 });
+
 // MUETRAS TODOS LOS CAMPEONES👇
 
+// Obtener el botón con el ID "todosLosCampeones"
 const btnCampeones = document.getElementById("todosLosCampeones");
-btnCampeones.addEventListener("click", () => {
-  card = [];
-  fullData.forEach((element) => {
-    // const element = data.data[i];
-    // element.slice(0, 19); //para limitarlo
 
+// Agregar un evento de escucha al hacer clic en el botón
+btnCampeones.addEventListener("click", () => {
+  // Vaciar el arreglo "card"
+  card = [];
+
+  // Iterar sobre cada elemento en "fullData"
+  fullData.forEach((element) => {
+    // Agregar contenido HTML al arreglo "card" usando template literals
     card.push(`
-    <div>
-    <h2>${element.name}</h2>
-    <img src="${element.img}" alt="img campeones"  width="${imgSize}">
-    </div>
+      <div class="containerCard">
+        <section class="card">
+          <h2>${element.name}</h2>
+          <span>${element.title}</span>
+          <img src="${element.splash}" alt="img campeones" class="pic"><button></button>
+        </section>
+      </div>
     `);
+
+    // Obtener el elemento del DOM con el ID "campeones-card"
     const contenedorCampeones = document.getElementById("campeones-card");
+
+    // Actualizar el contenido del elemento con el HTML generado por "card" utilizando el método "join"
     contenedorCampeones.innerHTML = card.join("");
   });
 });
 
-    
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
